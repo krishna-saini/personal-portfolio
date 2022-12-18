320113fe67be9122932b7e5c1fd786f2af2bd060
krishna-saini <krishnapraanjal10@gmail.com>
2022-12-18 15:05:03 +0530

    tech stack section add

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Head from "next/head";
+
 import {
   AiFillTwitterCircle,
   AiFillLinkedin,
   AiFillYoutube,
   AiFillGithub,
 } from "react-icons/ai";
-import { FaBlogger} from "react-icons/fa";
+import { FaBlogger } from "react-icons/fa";
 
-import { BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from "react";
 import avatar1 from "../public/avatar1.jpeg";
 import code from "../public/code.png";
@@ -20,6 +20,34 @@ import web3 from "../public/web3.png";
 import web4 from "../public/web4.png";
 import web5 from "../public/web5.png";
 import web6 from "../public/web6.png";
+
+import Link from "next/link";
+
+const socialLinks = [
+  {
+    icon: <AiFillTwitterCircle />,
+    href: "https://twitter.com/krishnasaini1",
+  },
+  {
+    icon: <AiFillLinkedin />,
+    href: "www.linkedin.com/in/krishna-saini-frontend-developer",
+  },
+  { icon: <AiFillGithub />, href: "https://github.com/krishna-saini" },
+  {
+    icon: <FaBlogger />,
+    href: "https://krishnasaini.hashnode.dev/",
+  },
+];
+
+const techStacks = [
+  { name: "JavaScript", href: "/skills/js.svg" },
+  { name: "React.js", href: "/skills/react.svg" },
+  { name: "CSS3", href: "/skills/css3.svg" },
+  { name: "Tailwind", href: "/skills/tailwind.svg" },
+  { name: "HTML5", href: "/skills/html5.svg" },
+  { name: "Next.js", href: "/skills/git.svg" },
+  { name: "Git", href: "/skills/git.svg" },
+];
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
@@ -52,67 +80,9 @@ export default function Home() {
       {/* Main  */}
       <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
         {/* hero section  */}
-        <section className="min-h-screen">
-          <nav className="py-10 mb-12 flex justify-between dark:text-white">
-            <h1 className="font-burtons text-xl">developedbyed</h1>
-            <ul className="flex items-center">
-              <li>
-                <BsFillMoonStarsFill
-                  onClick={() => setDarkMode(!darkMode)}
-                  className=" cursor-pointer text-2xl"
-                />
-              </li>
-              <li>
-                <a
-                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="#"
-                >
-                  Resume
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <div className="text-center p-10 py-10">
-            <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
-              Krishna Saini
-            </h2>
-            <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
-              Full Stack Developer with Specialization in Frontend
-            </h3>
-            <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
-              nesciunt eum suscipit eius asperiores, veniam minus amet quaerat
-              harum! Maxime.
-            </p>
-            <div className="text-5xl flex justify-center gap-16 py-3 ">
-              <a
-                href="https://twitter.com/krishnasaini1"
-                className="bg-stone-400 p-2  text-white  rounded-full"
-              >
-                <AiFillTwitterCircle />
-              </a>
-
-              <a
-                href="www.linkedin.com/in/krishna-saini-frontend-developer"
-                className="bg-stone-400 p-2  text-white  rounded-full"
-              >
-                <AiFillLinkedin />
-              </a>
-
-              <a
-                href="https://github.com/krishna-saini"
-                className="bg-stone-400 p-2  text-white  rounded-full"
-              >
-                <AiFillGithub />
-              </a>
-
-              <a
-                href="https://krishnasaini.hashnode.dev/"
-                className="bg-stone-400 p-2  text-white  rounded-full"
-              >
-                <FaBlogger/>
-              </a>
-            </div>
+        <section className="min-h-screen py-20">
+          <div className="flex items-center">
+            {/* photo - left section */}
             <div className="h-80 w-80 bg-gradient-to-b from-teal-500  rounded-full mt-20 mx-auto relative overflow-hidden  md:h-96 md:w-96">
               <Image
                 src={avatar1}
@@ -121,147 +91,68 @@ export default function Home() {
                 style={{ objectFit: "cover" }}
               />
             </div>
+            {/* right hero section  */}
+            <div className="text-center p-10 py-10">
+              <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
+                Hi, I am Krishna
+              </h2>
+              <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
+                A Full Stack Developer with Specialization in Frontend
+              </h3>
+              <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
+                nesciunt eum suscipit eius asperiores, veniam minus amet quaerat
+                harum! Maxime.
+              </p>
+              {/* social links  */}
+              <div className="text-5xl flex justify-center gap-16 py-3 ">
+                {/* different section list  */}
+                <ul className="flex items-center justify-between gap-20 font-burtons ">
+                  {socialLinks.map((link, index) => {
+                    return (
+                      <li
+                        key={index}
+                        className="bg-stone-400 p-2  text-white  rounded-full"
+                      >
+                        <a href={link.href}>{link.icon}</a>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
           </div>
         </section>
-        {/* <section>
+
+        {/* skills */}
+        <section className="">
           <div>
-            <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I&apso;ve done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
-            </p>
-          </div>
-          <div className="lg:flex gap-10">
-            <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
-              <Image src={design} width={100} height={100} alt="" />
-              <h3 className="text-lg font-medium pt-8 pb-2  ">
-                Beautiful Designs
-              </h3>
-              <p className="py-2">
-                Creating elegant designs suited for your needs following core
-                design theory.
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
-            </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={code} width={100} height={100} alt="" />
-              <h3 className="text-lg font-medium pt-8 pb-2 ">
-                Code your dream project
-              </h3>
-              <p className="py-2">
-                Do you have an idea for your next great website? Let&apso;s make
-                it a reality.
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
-            </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={consulting} width={100} height={100} alt="" />
-              <h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
-              <p className="py-2">
-                Are you interested in feedback for your current project? I can
-                give you tips and tricks to level it up.
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+            <h3 className="text-5xl text-center  text-teal-600 font-medium dark:text-teal-400 md:text-6xl ">Tech Stacks</h3>
+            <div className="my-16 grid  gap-16 items-center grid-cols-3 sm:grid-cols-5">
+              {/* skill 1  */}
+              {techStacks.map((stack, index) => {
+                return (
+                  <div key={index} className="flex flex-col items-center gap-2">
+                    <Image
+                      src={stack.href}
+                      alt={stack.name}
+                      width={80}
+                      height={80}
+                    />
+                    <p className="font-semibold ">{stack.name}</p>
+                  </div>
+                );
+              })}
             </div>
           </div>
-        </section> */}
-        <section className="py-10">
+        </section>
+
+        {/* top project   */}
+        <section>
           <div>
-            <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I&aposve done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
-            </p>
-          </div>
-          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-            <div className="basis-1/3 flex-1 ">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                // responsive="true"
-                src={web1}
-                alt=""
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                // responsive="true"
-                src={web2}
-                alt=""
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                // responsive="true"
-                src={web3}
-                alt=""
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                // responsive="true"
-                src={web4}
-                alt=""
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                // responsive="true"
-                src={web5}
-                alt=""
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                // responsive="true"
-                src={web6}
-                alt=""
-              />
-            </div>
+            <h3 className="text-3xl py-1 dark:text-white ">
+              featured projects
+            </h3>
           </div>
         </section>
       </main>
